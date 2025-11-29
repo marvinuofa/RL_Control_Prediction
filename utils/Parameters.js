9b5ab95df62d65b8f8a2f3feed95304fc592b80c
marvinuofa <mcu@ualberta.ca>
2025-11-29 00:03:46 -0700

    made base file, set up directories

---
+++ utils/Parameters.js
@@ -0,0 +1,8 @@
+export class Parameters {
+    constructor({ alpha=0.1, gamma=0.99, epsilon=0.1 }) {
+      this.alpha = alpha;
+      this.gamma = gamma;
+      this.epsilon = epsilon;
+    }
+  }
+  
